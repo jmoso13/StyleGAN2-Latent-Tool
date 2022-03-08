df3f05aea4ad0bd5cf3f91e166461429f20077af
Jeff Sontag <jsontag@tala.co>
2022-03-08 15:07:02 -0800

    __init__ add

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_git_version():
   return version
 
 setup(
-  name='lttool',
+  name='js-latent-utils',
   version=get_git_version(),
   author='Jeff Sontag',
   url='https://github.com/jmoso13/StyleGAN2-Latent-Tool',
